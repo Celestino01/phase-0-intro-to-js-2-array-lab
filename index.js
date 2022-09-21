c1ffaedc6a97553a7aa5c216f91f803430d2fd9c
celestino <celestino.martinez01@gmail.com>
2022-09-21 13:34:12 -0400

    commit tree 9994e12f8641f8557081ff673bb64e5df9b43503

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    const copyCat = [...cats]
+    copyCat.push(name)
+    return copyCat
+}
+
+function prependCat(name){
+    const copyCat = [...cats]
+    copyCat.unshift(name)
+    return copyCat
+}
+
+function removeLastCat(){
+    const copyCat = [...cats]
+    copyCat.pop()
+    return copyCat
+}
+
+function removeFirstCat(){
+    const copyCat = [...cats]
+    copyCat.shift()
+    return copyCat
+}
